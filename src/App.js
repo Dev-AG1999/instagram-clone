d1b41c4aeb63d9b9dd9439cc16d4b9075fe5458e
Aishwariya Ghosal <aishwariya.ghosal04@gmail.com>
2022-10-16 01:31:56 +0530

    changed

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import AddToPhotosIcon from "@mui/icons-material/AddToPhotos";
 
 function App() {
   const [posts, setPosts] = useState([]);
-  const [open, setOpen] = useState(false);
+  const [setOpen] = useState(false);
   const [openSignIn, setOpenSignIn] = useState(false);
   const [openImageUpload, setOpenImageUpload] = useState(false);
   const [email, setEmail] = useState("");
