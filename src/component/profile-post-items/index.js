93a0787adb25f3dc42a8864394d05239b642e023
Aishwariya Ghosal <aishwariya.ghosal04@gmail.com>
2022-11-06 18:18:31 +0530

    fixes

--- src/component/profile-post-items/index.js
+++ src/component/profile-post-items/index.js
@@ -1,7 +1,7 @@
-import react from 'react'
+
  export const ProfilePostItems=({src})=>{
     return(
-        <img src={src} style={{height:"100px",width:"100px",margin:"8px",objectFit:"cover"}}>
+        <img alt="" src={src} style={{height:"100px",width:"100px",margin:"8px",objectFit:"cover"}}>
 
         </img>
     )
